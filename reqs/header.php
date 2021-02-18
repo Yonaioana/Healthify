26f9d9594db95b5637c7f2506230f8d8603d8f63
Vasiliu Diana <diana.vasiliu10@gmail.com>
2021-02-19 00:08:41 +0200

    added first pages

---
+++ reqs/header.php
@@ -0,0 +1,52 @@
+<header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-3">
+                            <div class="logo-img">
+                                <a href="index.php">
+                                    <img src="img/logo2.png" alt="" id="logo">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-9 col-lg-9">
+                            <div class="menu_wrap d-none d-lg-block">
+                                <div class="menu_wrap_inner d-flex align-items-center justify-content-end">
+                                    <div class="main-menu">
+                                        <nav>
+                                            <ul id="navigation">
+                                                <li><a href="index.php">home</a></li>
+                                                <li><a href="about.php">About</a></li>
+                                                <li><a href="#">blog <i class="ti-angle-down"></i></a>
+                                                    <ul class="submenu">
+                                                        <li><a href="blog.php">blog</a></li>
+                                                        <li><a href="single-blog.php">single-blog</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="#">pages <i class="ti-angle-down"></i></a>
+                                                    <ul class="submenu">
+                                                        <li><a href="department.php">Deportment</a></li>
+                                                        <li><a href="elements.php">elements</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="contact.php">Contact</a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                    <div class="book_room">
+                                        <div class="book_btn">
+                                            <a class="popup-with-form" href="#test-form">Book Appointment</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
